f13422280d86e2ed1456cdad787151d144e47b79
MohamedAzeem98 <azeemerv@gmail.com>
2023-07-27 20:41:37 +0530

    First imp

---
+++ src/Practicing/facting123.java
@@ -0,0 +1,24 @@
+package Practicing;
+
+public class facting123 {
+	public static void main(String[] args) {
+		
+		
+		int n=7;
+		long fact=1;
+		
+		for(int i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+		System.out.println("The Factorial of "+n+"  is "+fact);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
